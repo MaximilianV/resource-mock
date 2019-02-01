55a1581bb059f6b2034e4bd1a562a046ce27b7a3
MaximilianV <maximilian-voelker@t-online.de>
2019-02-01 13:41:12 +0100

    Add first resource route

---
+++ README.md
@@ -0,0 +1,6 @@
+# Resource Manager Mock
+
+1. Clone this repository
+1. Run `npm install`
+1. Start the mock server with `npm start`
+1. Server listens on port 3000
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var resources = require('./routes/resources.js')
+
+app.get('/', function (req, res) {
+  res.send('Look at /resources!');
+});
+
+app.use('/resources', resources);
+
+app.listen(3000, function () {
+  console.log('Resource-mock listening on port 3000!');
+});
